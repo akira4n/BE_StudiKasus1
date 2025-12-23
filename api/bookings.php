fb96facc98b3e9d805ede2c1277bc613163a7b91
akira4n <yobiijo94@gmail.com>
2025-12-23 21:51:21 +0700

    feat(bookings): add patch for status)

--- api/bookings.php
+++ api/bookings.php
@@ -47,4 +47,44 @@ if ($method === 'GET') {
         http_response_code(400);
         echo json_encode(["error" => $e->getMessage()]);
     }
+} elseif ($method === 'PATCH') {
+    // patch ubah status
+    $booking_id = $input['id'] ?? null;
+    $new_status = $input['status'] ?? null;
+
+    if (!$booking_id || !$new_status) {
+        http_response_code(400);
+        echo json_encode(["message" => "id booking dan status baru harus diisi"]);
+        exit;
+    }
+
+    try {
+        $pdo->beginTransaction();
+
+        $stmt = $pdo->prepare("SELECT event_id, status FROM bookings WHERE id = ? FOR UPDATE");
+        $stmt->execute([$booking_id]);
+        $old_booking = $stmt->fetch();
+
+        if (!$old_booking) {
+            throw new Exception("data booking tidak ditemukan");
+        }
+
+        // stok nya +1 kalo status berubah ke cancel/exp
+        if (($old_booking['status'] !== 'cancelled' && $old_booking['status'] !== 'expired') &&
+            ($new_status === 'cancelled' || $new_status === 'expired')
+        ) {
+            $updateStock = $pdo->prepare("UPDATE events SET remaining_stock = remaining_stock + 1 WHERE id = ?");
+            $updateStock->execute([$old_booking['event_id']]);
+        }
+
+        $updateStatus = $pdo->prepare("UPDATE bookings SET status = ? WHERE id = ?");
+        $updateStatus->execute([$new_status, $booking_id]);
+
+        $pdo->commit();
+        echo json_encode(["message" => "Status booking berhasil diperbarui!"]);
+    } catch (Exception $e) {
+        $pdo->rollBack();
+        http_response_code(400);
+        echo json_encode(["error" => $e->getMessage()]);
+    }
 }
